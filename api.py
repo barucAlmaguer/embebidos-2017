ecd1c5f6b07f9991a766939c47b83995d7d3afab
baruc almaguer <baruc.almaguer@gmail.com>
2017-11-08 00:12:04 -0600

    vista de tabla 2

--- api.py
+++ api.py
@@ -33,7 +33,9 @@ def logging(value):
 def view_table(value):
     response = ""
     cur = conn.cursor()
-    cur.execute("SELECT dato from RANDOM;");
-    for i, dato in enumerate(cur):
+    cur.execute("SELECT dato from RANDOM ORDER BY id desc;");
+    for i, dato[0] in enumerate(cur):
+        if i >= value:
+            break
         response += "{}={}<br>".format(i, dato)
     return response
