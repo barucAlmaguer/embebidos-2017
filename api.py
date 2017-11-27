a71691fe95ec378b585f61bfdd2d1b07547de3f1
baruc <baruc.almaguer@gmail.com>
2017-11-26 19:30:52 -0600

    ejemplo chart html + js

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import jsonify
+from flask import Markup
+from flask import Flask
+from flask import render_template
 
 import os
 from urllib import parse
@@ -50,3 +53,12 @@ def view_json(value):
         k, v = res.split('=')
         d[k] = v
     return jsonify(d)
+
+@app.route("/chart/")
+def chart():
+    labels = ["January","February","March","April","May","June","July","August"]
+    values = [10,9,8,7,6,4,7,8]
+    return render_template('chart.html', values=values, labels=labels)
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
